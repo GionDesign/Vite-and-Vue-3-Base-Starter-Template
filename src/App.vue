94c6a3b180e5f2c6a8727513f271f440d349e08a
Pascal <s3682040@student.rmit.edu.au>
2022-03-04 08:00:01 +1100

    updated to correctly add tailwindcss to postcss config

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img class="m-auto" alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
   <!-- Uncomment the below router-view tag to use vue-router -->
   <!-- <router-view /> -->
